696fbc068b060fb6283a4960b9d7baf7148c219f
EduardoGP <124112815+Eduardo7052311@users.noreply.github.com>
2024-11-28 22:29:31 +0000

    actualizacion generador de excusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function aleatorio(lista) {
+    let numAleatorio = Math.floor(Math.random() * lista.length);
+    return lista[numAleatorio] + " ";
+  }
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let excuse =
+    aleatorio(who) + aleatorio(action) + aleatorio(what) + aleatorio(when);
+
+  let elementExcuse = window.document.querySelector("#excuse");
+  elementExcuse.innerHTML = excuse;
 };
